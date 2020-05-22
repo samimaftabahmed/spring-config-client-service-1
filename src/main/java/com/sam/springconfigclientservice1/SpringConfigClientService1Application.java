873884d4440540e67246a35c18b8412f93375f39
Samim <samimaftabahmed@gmail.com>
2020-05-22 23:45:12 +0530

    First Commit

---
+++ src/main/java/com/sam/springconfigclientservice1/SpringConfigClientService1Application.java
@@ -0,0 +1,13 @@
+package com.sam.springconfigclientservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigClientService1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfigClientService1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sam/springconfigclientservice1/SpringConfigClientService1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.springconfigclientservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigClientService1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
