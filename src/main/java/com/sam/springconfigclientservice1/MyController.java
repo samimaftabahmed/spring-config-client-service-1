da2b0f980c19390331f5cf68de452ab91efb63b8
Samim <samimaftabahmed@gmail.com>
2020-05-24 14:35:07 +0530

    added refresh() URL

--- src/main/java/com/sam/springconfigclientservice1/MyController.java
+++ src/main/java/com/sam/springconfigclientservice1/MyController.java
@@ -2,6 +2,7 @@ package com.sam.springconfigclientservice1;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -20,11 +21,20 @@ public class MyController {
         return dbSettings.toString();
     }
 
+    // Put http basic security in this mapping
     @GetMapping("/refresh")
     public void refresh() {
-        RestTemplate restTemplate = new RestTemplate();
-        HttpEntity<String> request = new HttpEntity<>("");
+
         String url = "http://localhost:" + serverSettings.getPort() + "/actuator/refresh";
-        restTemplate.postForObject(url, request, String.class);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        headers.set("Accept", "application/json");
+
+        HttpEntity<String> httpEntity = new HttpEntity<>("", headers);
+
+        RestTemplate restTemplate = new RestTemplate();
+        Object o = restTemplate.postForObject(url, httpEntity, String.class);
+        System.out.println(o);
     }
 }
